34ebf776da2e5445087d28ff5119e938c6445612
Adrian Hall <adrian@shellmonger.com>
2015-03-23 17:25:44 -0700

    AureliaTutorial t-3

--- AureliaTutorial/Gulpfile.js
+++ AureliaTutorial/Gulpfile.js
@@ -1,2 +1,20 @@
-﻿var gulp = require("gulp");
+/// <binding BeforeBuild='_build' />
+var gulp = require("gulp"),
+    babel = require("gulp-babel"),
+    sourcemaps = require("gulp-sourcemaps");
+
+gulp.task('build:js', function () {
+    return gulp.src("src/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(babel({ modules: "system" }))
+        .pipe(sourcemaps.write({ includeContent: false, sourceRoot: "/src/" }))
+        .pipe(gulp.dest("wwwroot/"));
+});
+
+gulp.task('build:html', function () {
+    return gulp.src("src/**/*.html")
+        .pipe(gulp.dest("wwwroot/"));
+});
+
+gulp.task("_build", ["build:js", "build:html"]);
 
