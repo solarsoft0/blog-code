68731e50078e10ddc111c95864ad4f74d114405b
Adrian Hall <adrian@shellmonger.com>
2015-03-25 16:39:55 -0700

    AspNetIdentity v.3

--- AspNetIdentity/Controllers/AccountController.cs
+++ AspNetIdentity/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Mvc;
 
 using AspNetIdentity.Models;
 using AspNetIdentity.ViewModels;
+using System.Diagnostics;
 
 namespace AspNetIdentity.Controllers
 {
@@ -30,6 +31,7 @@ namespace AspNetIdentity.Controllers
             private set;
         }
 
+        #region /Account/Login
         /**
          * GET /Account/Login
          */
@@ -41,6 +43,9 @@ namespace AspNetIdentity.Controllers
             return View();
         }
 
+        /**
+         * POST /Account/Login
+         */
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
@@ -56,18 +61,83 @@ namespace AspNetIdentity.Controllers
                 }
                 if (result.IsLockedOut)
                 {
-                    return View("Lockout");
+                    ModelState.AddModelError("", "Locked Out");
+                }
+                else if (result.IsNotAllowed)
+                {
+                    ModelState.AddModelError("", "Not Allowed");
+                }
+                else if (result.RequiresTwoFactor)
+                {
+                    ModelState.AddModelError("", "Requires Two-Factor Authentication");
                 }
                 else
                 {
                     ModelState.AddModelError("", "Invalid username or password.");
-                    return View(model);
                 }
+                return View(model);
             }
 
             // If we got this far, something failed - redisplay the form
             return View(model);
         }
+        #endregion
+
+        #region /Account/Register
+        /**
+         * GET: /Account/Register
+         */
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        /**
+         * POST: /Account/Register
+         */
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Debug.WriteLine("Register: Creating new ApplicationUser");
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                Debug.WriteLine(string.Format("Register: New Application User = {0}", user.UserName));
+                var result = await UserManager.CreateAsync(user, model.Password);
+                Debug.WriteLine(string.Format("Register: Registration = {0}", result.Succeeded));
+                if (result.Succeeded)
+                {
+                    // Success - redirect to the login page
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    Debug.WriteLine(string.Format("Register: Adding Error: {0}:{1}", error.Code, error.Description));
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+            // Somethign went wrong, but we don't know what
+            return View(model);
+        }
+        #endregion
+
+        #region Logoff
+        /**
+         * POST: /Account/Logout
+         */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            SignInManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
 
         #region Helpers
         private IActionResult RedirectToLocal(string returnUrl)
@@ -75,7 +145,8 @@ namespace AspNetIdentity.Controllers
             if (Url.IsLocalUrl(returnUrl))
             {
                 return Redirect(returnUrl);
-            } else
+            }
+            else
             {
                 return RedirectToAction("Index", "Home");
             }
