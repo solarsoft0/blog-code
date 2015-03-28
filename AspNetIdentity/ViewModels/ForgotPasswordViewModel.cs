ab6e0466ee395d9db7ab7d842191f5e673a5f017
Adrian Hall <adrian@shellmonger.com>
2015-03-27 23:55:42 -0700

    AspNetIdentity v.5

---
+++ AspNetIdentity/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetIdentity.ViewModels
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
