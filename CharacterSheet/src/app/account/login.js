e93bc2180d554d10dfbe528f1d81738f6b95f8b2
Adrian Hall <adrian@shellmonger.com>
2015-04-04 18:00:21 -0700

    Removing CharacterSheet project

--- CharacterSheet/src/app/account/login.js
+++
@@ -1,9 +0,0 @@
-﻿import {LogManager} from "aurelia-framework";
-
-var logger = LogManager.getLogger("app/account/login");
-
-export class Login {
-    constructor() {
-        logger.debug("[Login::constructor]");
-    }
-}
