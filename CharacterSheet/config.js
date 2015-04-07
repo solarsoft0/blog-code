5cc845c18cf450b2f0de5795c0ab2f66941b625b
Adrian Hall <adrian@shellmonger.com>
2015-04-06 23:05:29 -0700

    cs-0.0.2 - Added Polymer and webcomponents.js

--- CharacterSheet/config.js
+++ CharacterSheet/config.js
@@ -15,14 +15,25 @@ System.config({
 
 System.config({
   "map": {
+    "Polymer/polymer": "github:Polymer/polymer@0.8.0",
     "babel": "npm:babel@4.7.16",
     "babel-runtime": "npm:babel-runtime@4.7.16",
     "font-awesome": "npm:font-awesome@4.3.0",
     "jquery": "github:components/jquery@2.1.3",
+    "webcomponents.js": "npm:webcomponents.js@0.6.0",
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@4.7.16": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:webcomponents.js@0.6.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
