6fd4b7564573687813f64443e713c61c8d4c1384
Adrian Hall <adrian@shellmonger.com>
2015-04-08 13:30:41 -0700

    cs-0.0.4 The dnd-gridcharacter web component

--- CharacterSheet/config.js
+++ CharacterSheet/config.js
@@ -18,6 +18,7 @@ System.config({
     "Polymer/polymer": "github:Polymer/polymer@0.8.0",
     "babel": "npm:babel@4.7.16",
     "babel-runtime": "npm:babel-runtime@4.7.16",
+    "bootstrap": "github:twbs/bootstrap@3.3.4",
     "font-awesome": "npm:font-awesome@4.3.0",
     "jquery": "github:components/jquery@2.1.3",
     "webcomponents.js": "npm:webcomponents.js@0.6.0",
@@ -26,6 +27,9 @@ System.config({
     },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
+    },
+    "github:twbs/bootstrap@3.3.4": {
+      "jquery": "github:components/jquery@2.1.3"
     },
     "npm:babel-runtime@4.7.16": {
       "process": "github:jspm/nodelibs-process@0.1.1"
---
+++ CharacterSheet/src/elements/dnd-gridcharacter/dnd-gridcharacter.js
@@ -0,0 +1,56 @@
+﻿Polymer({
+    is: "dnd-gridcharacter",
+
+    properties: {
+        party: {
+            type: Boolean,
+            value: false,
+            observer: "partyObserver"
+        },
+        shared: {
+            type: Boolean,
+            value: false,
+            observer: "sharedObserver"
+        }
+    },
+
+    //#region Global Event Listeners
+    domReady: function() {
+        this.windowResize();
+    },
+
+    windowResize: function() {
+        // Make the object square by setting height == width
+        this.style.height = this.offsetWidth + "px";
+
+        // Make the character name font fit inside the containing DIV
+        var textHeight = Math.floor(this.offsetHeight * 0.12);
+        Polymer.dom(this).querySelector("h1").style.fontSize = textHeight + "px";
+    },
+    //#endregion
+
+    //#region Lifecycle event handlers
+    ready: function() {
+        var that = this;
+        // Add event handler for DOMContentLoaded
+        if (document.readyState != 'loading') {
+            this.domReady();
+        } else {
+            window.addEventListener('DOMContentLoaded', function () { that.domReady(); });
+        }
+
+        // Add event handler for window resize
+        window.addEventListener("resize", function() { that.windowResize(); });
+    },
+    //#endregion
+
+    //#region Property Observers
+    partyObserver: function (newValue, oldValue) {
+        this.$.partyIcon.style.display = newValue ? "block" : "none";
+    },
+
+    sharedObserver: function (newValue, oldValue) {
+        this.$.shareIcon.style.display = newValue ? "block" : "none";
+    }
+    //#endregion
+});
