25e4fff8e1426dd2c1f1d644706db96161f7a30d
Adrian Hall <adrian@shellmonger.com>
2015-07-02 11:30:46 -0700

    Initial version of the EmberJS Tutorial Project

---
+++ EmberTutorial/Gulpfile.js
@@ -0,0 +1,23 @@
+﻿var gulp = require("gulp"),
+    less = require("gulp-less");
+
+var config = {
+    src: "./Client",
+    dest: "./wwwroot"
+};
+
+gulp.task("default", [
+    "htmlfiles",
+    "stylesheets"
+]);
+
+gulp.task("htmlfiles", function () {
+    return gulp.src(config.src + "/**/*.html")
+        .pipe(gulp.dest(config.dest));
+});
+
+gulp.task("stylesheets", function () {
+    return gulp.src(config.src + "/Style/site.less")
+        .pipe(less())
+        .pipe(gulp.dest(config.dest));
+});
