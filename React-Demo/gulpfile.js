0173bb108239129286a3d232c0285e56aefc7cdb
Adrian Hall <adrian@shellmonger.com>
2015-07-21 11:50:35 -0700

    Initial Web Server configuration.

---
+++ React-Demo/gulpfile.js
@@ -0,0 +1,27 @@
+﻿/// <binding BeforeBuild='build' />
+var gulp = require('gulp'),
+    less = require('gulp-less');
+
+var config = {
+    src: './Client',
+    dest: './wwwroot'
+};
+
+var files = {
+    html: config.src + '/**/*.html',
+    less: config.src + '/style/bundle.less'
+};
+
+gulp.task('build', ['style', 'html']);
+
+gulp.task('html', function ()
+{
+    return gulp.src([files.html]).pipe(gulp.dest(config.dest));
+});
+
+gulp.task('style', function ()
+{
+    return gulp.src([files.less])
+        .pipe(less())
+        .pipe(gulp.dest(config.dest));
+});
