4108a8237a0384d7ac0fafa06e1d35a690719ea8
Adrian Hall <adrian@shellmonger.com>
2016-09-19 12:03:45 -0700

    Initial Checking ASP.NET MVC + Mobile

---
+++ asp-net-mvc-mobile-apps/WebApplication1/App_Start/MobileAppsConfig.cs
@@ -0,0 +1,51 @@
+﻿using System.Configuration;
+using System.Data.Entity;
+using System.Web.Http;
+using WebApplication1.Models;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Authentication;
+using Microsoft.Azure.Mobile.Server.Config;
+using Owin;
+
+namespace WebApplication1
+{
+    public partial class Startup
+    {
+        public static void ConfigureMobileApp(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            new MobileAppConfiguration()
+                .AddTablesWithEntityFramework()
+                .ApplyTo(config);
+
+            config.MapHttpAttributeRoutes();
+
+            Database.SetInitializer(new MobileServiceInitializer());
+
+            #region Local Development Settings
+            MobileAppSettingsDictionary settings = config.GetMobileAppSettingsProvider().GetMobileAppSettings();
+            if (string.IsNullOrEmpty(settings.HostName))
+            {
+                app.UseAppServiceAuthentication(new AppServiceAuthenticationOptions
+                {
+                    SigningKey = ConfigurationManager.AppSettings["SigningKey"],
+                    ValidAudiences = new[] { ConfigurationManager.AppSettings["ValidAudience"] },
+                    ValidIssuers = new[] { ConfigurationManager.AppSettings["ValidIssuer"] },
+                    TokenHandler = config.GetAppServiceTokenHandler()
+                });
+            }
+            #endregion
+
+            app.UseWebApi(config);
+        }
+    }
+
+    public class MobileServiceInitializer : CreateDatabaseIfNotExists<MobileServiceContext>
+    {
+        protected override void Seed(MobileServiceContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
---
+++ asp-net-mvc-mobile-apps/WebApplication1/Models/MobileServiceContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using Microsoft.Azure.Mobile.Server.Tables;
+using WebApplication1.DataObjects;
+
+namespace WebApplication1.Models
+{
+    public class MobileServiceContext : DbContext
+    {
+        private const string connectionStringName = "Name=MS_TableConnectionString";
+
+        public MobileServiceContext() : base(connectionStringName)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Add(
+                new AttributeToColumnAnnotationConvention<TableColumnAttribute, string>(
+                    "ServiceColumnTable", (property, attributes) => attributes.Single().ColumnType.ToString()));
+        }
+    }
+}
